3babc935e8f0592be2409e26c170260dfe1089c2
swapnil <117852783+swapnil-58@users.noreply.github.com>
2023-01-14 21:22:00 +0530

    Update BlogService.java

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -26,12 +26,20 @@ public class BlogService {
 
     public List<Blog> showBlogs(){
         //find all blogs
+        return  blogRepository1.findAll();
 
     }
 
     public void createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        Blog blog=new Blog(title,content,new Date());
+        blog.setUser(userRepository1.findById(userId).get());
+        User user=userRepository1.findById(userId).get();
+        List<Blog> res=user.getBlogList();
+        res.add(blog);
+        user.setBlogList(res);
+        blogRepository1.save(blog);
+        userRepository1.save(user);
         //updating the blog details
 
         //Updating the userInformation and changing its blogs
@@ -40,13 +48,27 @@ public class BlogService {
 
     public Blog findBlogById(int blogId){
         //find a blog
+        return blogRepository1.findById(blogId).get();
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
+        Blog blog=blogRepository1.findById(blogId).get();
+        Image image=imageService1.createAndReturn(blog,description,dimensions);
+        image.setBlog(blog);
+        List<Image> imageList=blog.getImageList();
+        if(imageList==null) imageList=new ArrayList<>();
+        imageList.add(image);
+        blog.setImageList(imageList);
+        blogRepository1.save(blog);
+
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        if(blogRepository1.findById(blogId).get()==null) return;
+        blogRepository1.deleteById(blogId);
+
+
     }
 }
